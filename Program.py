478167802d716822a08caa7edb44efd9a60a2449
mihoa <donovangilliam45@gmail.com>
2024-08-24 20:13:17 -0400

    list

--- Program.py
+++ Program.py
@@ -1,18 +1,26 @@
-import bs4
 from bs4 import BeautifulSoup
 import requests
 
 
 class ProductInfo:
-    productName = ""
-    productPrice = ""
+    def __init__(self, productname, productprice):
+        self.productName = productname
+        self.productPrice = productprice
 
+
+ProductList = []
 
 def parse_html():
     request = requests.get("https://www.jetpens.com/cPath/new")
     soup = BeautifulSoup(request.text,"html.parser")
-    arrival_text = soup.find("div", {"class" : "products pure-g margin-tb-10"})
-    print(arrival_text)
+
+    for item in soup.find_all(class_="pure-u-1-2 pure-u-sm-1-2 pure-u-md-1-3 pure-u-lg-1-4 product"):
+        product = item.find_next(class_="product-name subtle").text
+        price = item.find_next(class_="price").text
+        ProductList.append(ProductInfo(product, price))
+    for obj in ProductList:
+        print(obj.productName)
+        print(obj.productPrice)
 
 
 parse_html()
